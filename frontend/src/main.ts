5d0b24627b68508ae11fc47fa1be6502724363f8
Jc <107809331+y3eet@users.noreply.github.com>
2026-06-19 11:33:16 +0800

    feat: add go window type

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,11 @@
 import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
+declare global {
+  interface Window {
+    go: { main: { App: {} } };
+  }
+}
 
 const app = mount(App, {
   target: document.getElementById("app")!,
